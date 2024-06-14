a2d2bb1de8d0848319d75b1fd398d900418f8736
ynglchxx <avilaluisvallejos23@gmail.com>
2024-06-14 13:28:07 -0400

    lectura

--- Proyecto.py
+++ Proyecto.py
@@ -1,19 +1,30 @@
 #Autor(es): Vicente Ramirez 'El crack´ y Luis Vallejos 'El maquina' juntos son el "Duo Dinamita".
 def lectura_datos(proyecto):
-    f = open("proyecto.txt", 'r')
+    f= open(proyecto)
+    datos=[]
     for linea in f:
-        datos.append(linea.strip().split(','))
-        return datos
+        linea= linea.rstrip("\n")
+        lista = linea.split(",")
+        datos.append(lista)
+    f.close
+    return datos
+def funcion_a(datos):
+    pass
+def funcion_b(datos):
+    pass
+def funcion_c(datos):
+    pass
+def funcion_d(datos):
+    pass
+def funcion_e(datos):
+    pass
 
-
-
-
-if __name__ =="__main__":
-    datos= lectura_datos("proyecto.txt")
+if __name__ == "__main__":
+    datos= lectura_datos("protocolo_vigilancia.txt")
     print(datos)
-    positivos = funcion_a(casos_positivos) #casos_positivos(datos)
-    pos_febr = funcion_b(casospos_febr)  #casospos_febr(datos)
-    pelicano= funcion_c(casos_pelicanos) #casos_pelicanos(datos)
-    loro = funcion_d(casosloro_marzo) #casosloro_marzo(datos)
-    grafico = funcion_e(grafico) #grafico(datos)
-    generar_salida(datos,positivos,pos_febr,pelicano,loro,grafico)
+    positivos = funcion_a(datos) #casos_positivos
+    pos_febr = funcion_b(datos)  #casospos_febr(datos)
+    pelicano= funcion_c(datos) #casos_pelicanos(datos)
+    loro = funcion_d(datos) #casosloro_marzo(datos)
+    grafico = funcion_e(datos) #grafico(datos)
+    generar_salida(positivos,pos_febr,pelicano,loro,grafico)
