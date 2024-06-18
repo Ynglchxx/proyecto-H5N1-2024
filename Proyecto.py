90ef68cacaad94e989d826be11614b2266836940
ynglchxx <avilaluisvallejos23@gmail.com>
2024-06-17 21:10:18 -0400

    a y b resueltas

--- Proyecto.py
+++ Proyecto.py
@@ -9,9 +9,31 @@ def lectura_datos(proyecto):
     f.close
     return datos
 def funcion_a(datos):
+    lista = []
+    for i in datos:
+        region = i[3]
+        if region not in lista :    #Para la lista si ya esta
+            lista.append(region)
+
+    cant_reg=[]
+    for f in lista :
+        cant=0
+        for i in datos :
+            if i[3] == f and i[9] == 'Positivo':    #primero se busca la region y se ve si el caso es positivo y se agrega 1
+                cant = cant + 1
+        cant_reg.append(cant)
+
+    return lista , cant_reg
+    
     pass
-def funcion_b(datos):
-    pass
+def funcion_b(datos): #Casos de Febrero
+    cantidad = 0
+    for casof in datos:
+        fecha = casof[2].split("-")
+        if fecha[1] == "02" and fecha[2] == "2023" :
+            cantidad = cantidad + 1
+    return cantidad
+
 def funcion_c(datos):
     pass
 def funcion_d(datos):
@@ -21,10 +43,14 @@ def funcion_e(datos):
 
 if __name__ == "__main__":
     datos= lectura_datos("protocolo_vigilancia.txt")
-    print(datos)
+    #print(datos)
     positivos = funcion_a(datos) #casos_positivos
-    pos_febr = funcion_b(datos)  #casospos_febr(datos)
-    pelicano= funcion_c(datos) #casos_pelicanos(datos)
-    loro = funcion_d(datos) #casosloro_marzo(datos)
-    grafico = funcion_e(datos) #grafico(datos)
-    generar_salida(positivos,pos_febr,pelicano,loro,grafico)
+    print(positivos)
+    print(positivos[0][0]) #Regiones
+    print(positivos[1][0]) #Cantidades
+    pos_febr = funcion_b(datos)  #casospos_febr
+    print(pos_febr)
+    #pelicano= funcion_c(datos) #casos_pelicanos(datos)
+    #loro = funcion_d(datos) #casosloro_marzo(datos)
+    #grafico = funcion_e(datos) #grafico(datos)
+    #generar_salida(positivos,pos_febr,pelicano,loro,grafico)
