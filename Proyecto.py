ea159b9dd737083f3fce34bea009f0fb36503260
ynglchxx <avilaluisvallejos23@gmail.com>
2024-06-21 01:10:31 -0400

    Se logro ktm

--- Proyecto.py
+++ Proyecto.py
@@ -24,14 +24,15 @@ def funcion_a(datos): # Casos por regiones
                 cant = cant + 1
         cant_reg.append(cant)
 
-    return lista , cant_reg
-    pass
+    return lista , cant_reg, region
+
 def funcion_b(datos): # Casos de Febrero
     cantidad = 0
     for casof in datos:
         fecha = casof[2].split("-")
         if fecha[1] == "02" and fecha[2] == "2023" :
             cantidad = cantidad + 1
+
     return cantidad, casof
 def funcion_c(datos): # Casos de Pelicanos
     casos_pelicano = 0
@@ -46,8 +47,7 @@ def funcion_d(datos): # Casos de Loros
         fecha = casosl[2].split("-")
         if loro == "Loro" or "Loro Trincahue" and fecha[1] == "03" and fecha[2] == "2022" :
             cantidad = cantidad + 1
-        print(cantidad)    
-        return(cantidad)
+        return cantidad, casosl
 def funcion_e(datos): # Grafico               #Gaviota , Piquero, Salteador, Pelicano, Guanay
     gaviota = 0
     piquero = 0
@@ -67,36 +67,37 @@ def funcion_e(datos): # Grafico               #Gaviota , Piquero, Salteador, Pel
             guanay = guanay + 1
     return gaviota, piquero, salteador, pelicano, guanay
 def generar_salida(region, casosf, casos_pelicano, casosl) :
-    salida = open("ResultadoS3.txt", "w") as salida :   # Crear archivo de texto
+    salida = open("ResultadoS3.txt", "w", encoding="UTF-8" )               #Crear archivo de texto
     salida.write("Autor(es): Luis Vallejos Ávila, Vicente Ramírez Muñoz" + "\n")
     salida.write("Cantidad de casos detectados positivos Region" + "\n")
-    salida.write('\t' + "Región de Arica: " + str(region[3]) + "\n")
-    salida.write('\t' + "Región de Tarapaca:" + str(region[2])+ "\n")
-    salida.write('\t' + "Región de Antofagasta:" + str(region[4]) + "\n")
-    salida.write('\t' + "Región de Atacama:" + str(region[5]) + "\n")
-    salida.write('\t' + "Región de Coquimbo:" + str(region[7]) + "\n")
-    salida.write('\t' + "Región de Valparaíso:" + str(region[0]) + "\n")
-    salida.write('\t' + "Región de Metropolitana:" + str(region[9]) + "\n")
-    salida.write('\t' + "Región de O'Higgins:" + str(region[12]) + "\n")
-    salida.write('\t' + "Región del Maule:" + str(region[6]) + "\n")
-    salida.write('\t' + "Región de Ñuble:" + str(region[10]) + "\n")
-    salida.write('\t' + "Región del Biobío:" + str(region[8]) + "\n")
-    salida.write('\t' + "Región de La Araucanía:" + str(region[14]) + "\n")
-    salida.write('\t' + "Región de Los Ríos:" + str(region[13]) + "\n")
-    salida.write('\t' + "Región de Los Lagos:" + str(region[15]) + "\n")
-    salida.write('\t' + "Región de Aysén:" + str(region[11]) + "\n")
-    salida.write('\t' + "Región de Magallanes:" + str(region[1]) + "\n")
-    salida.write("Casos positivos mes Febrero del 2023: " + str(casosf) + '\n')
-    salida.write("Casos positivos especie Pelicanos: " + str(casos_pelicano + "\n"))
-    salida.write("Incidencias 03/2022 del" "Loro Trincahue Chileno: " + str(casosl) + "\n" )
+    salida.write('\t' + "Región de Arica: " + str(cant_reg[3]) + "\n")
+    salida.write('\t' + "Región de Tarapaca:" + str(cant_reg[2])+ "\n")
+    salida.write('\t' + "Región de Antofagasta:" + str(cant_reg[4]) + "\n")
+    salida.write('\t' + "Región de Atacama:" + str(cant_reg[5]) + "\n")
+    salida.write('\t' + "Región de Coquimbo:" + str(cant_reg[7]) + "\n")
+    salida.write('\t' + "Región de Valparaíso:" + str(cant_reg[0]) + "\n")
+    salida.write('\t' + "Región de Metropolitana:" + str(cant_reg[9]) + "\n")
+    salida.write('\t' + "Región de O'Higgins:" + str(cant_reg[12]) + "\n")
+    salida.write('\t' + "Región del Maule:" + str(cant_reg[6]) + "\n")
+    salida.write('\t' + "Región de Ñuble:" + str(cant_reg[10]) + "\n")
+    salida.write('\t' + "Región del Biobío:" + str(cant_reg[8]) + "\n")
+    salida.write('\t' + "Región de La Araucanía:" + str(cant_reg[14]) + "\n")
+    salida.write('\t' + "Región de Los Ríos:" + str(cant_reg[13]) + "\n")
+    salida.write('\t' + "Región de Los Lagos:" + str(cant_reg[15]) + "\n")
+    salida.write('\t' + "Región de Aysén:" + str(cant_reg[11]) + "\n")
+    salida.write('\t' + "Región de Magallanes:" + str(cant_reg[1]) + "\n")
+    salida.write("Casos positivos mes Febrero del 2023: " + str(cantidad) + '\n')
+    salida.write("Casos positivos especie Pelicanos: " + str(casos_pelicano) + "\n")
+    salida.write("Incidencias 03/2022 del " "Loro Trincahue Chileno: " + str(cantidad) + "\n" )
+    salida.close()
 
-    pass
+
 
 if __name__ == "__main__":
     datos= lectura_datos("protocolo_vigilancia.txt")
-    positivos = funcion_a(datos) #casos_positivos
-    pos_febr = funcion_b(datos)  #casospos_febr
-    pelicano = funcion_c(datos) #casos_pelicanos(datos)
-    loro = funcion_d(datos) #casosloro_marzo(datos)
+    lista, cant_reg, region = funcion_a(datos) #casos_positivos
+    cantidad, casof = funcion_b(datos)  #casospos_febr
+    casos_pelicano = funcion_c(datos) #casos_pelicanos(datos)
+    casosl = funcion_d(datos) #casosloro_marzo(datos)
     grafico = funcion_e(datos) #grafico(datos)
-    generar_salida(positivos,pos_febr,pelicano,loro,grafico)
+    salida= generar_salida(region,casof,casos_pelicano,casosl,)
